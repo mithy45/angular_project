5c684a1c70095b51889c96df0e9aa62e8831be49
Theo Billet <billettheo5@gmail.com>
2022-12-14 10:02:45 +0100

    home screen wip

--- AngularS3/src/app/app.module.ts
+++ AngularS3/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ArtistSearchComponent } from './artist-search/artist-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    ArtistSearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # angular_project
+npm install
+npm install bootstrap --save
+npm i bootstrap-icons
