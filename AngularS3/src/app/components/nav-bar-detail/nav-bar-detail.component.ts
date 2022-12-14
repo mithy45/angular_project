735adea290b7917d5eb6168b5cada1aefcdb7930
Brie DUMONT <brie_dumont@outlook.fr>
2022-12-14 16:09:19 +0100

    structure site

--- AngularS3/src/app/components/nav-bar-detail/nav-bar-detail.component.ts
+++ AngularS3/src/app/components/nav-bar-detail/nav-bar-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar-detail',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarDetailComponent implements OnInit {
 
-  constructor() { }
+  links = {
+    "/details/artist" : "Artist",
+    "/details/concert" : "Concert",
+    "/details/album" : "Album",
+    "/details/musique" : "Music",
+  };
+
+  currentRoute = "";
+  
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.currentRoute = this.router.url
   }
 
 }
