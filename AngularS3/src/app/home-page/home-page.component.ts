58fe445aa0aa904824219598ccf6c04b52ad03c8
Theo Billet <billettheo5@gmail.com>
2022-12-15 14:20:16 +0100

    search form finish

--- AngularS3/src/app/home-page/home-page.component.ts
+++ AngularS3/src/app/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
   selector: 'app-home-page',
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
+  checkoutForm: FormGroup;
 
-  constructor() { }
+  selectValue: string = "artist";
+  displayDateConcert: boolean = false;
+  
+  constructor(private formBuilder: FormBuilder) {
+    this.checkoutForm = this.formBuilder.group({
+      filter: this.selectValue,
+      search: '',
+      dateMin: null,
+      dateMax: null
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  onChangeSelect(opt: any) {
+    this.selectValue = opt.target.value;
+    if(opt.target.value === 'concert'){
+      this.displayDateConcert = true;
+    } else {
+      this.displayDateConcert = false;
+      this.checkoutForm.patchValue({
+                                  dateMin: null,
+                                  dateMax: null
+                                });
+    }
+  }
+
+  onSubmit() {
+    console.log("filter value : " + this.checkoutForm.get('filter')?.value);
+    console.log("search value : " + this.checkoutForm.get('search')?.value);
+    console.log("dateMin value : " + this.checkoutForm.get('dateMin')?.value);
+    console.log("dateMax value : " + this.checkoutForm.get('dateMax')?.value);
   }
 
 }
