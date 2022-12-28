61fdc013ba29117c29ad3b4993c530d247ec04e4
Brice DUMONT <brice.dumont@epita.fr>
2022-12-28 13:16:55 +0100

    musics page

--- AngularS3/src/app/pages/detail-albums/detail-albums.component.ts
+++ AngularS3/src/app/pages/detail-albums/detail-albums.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-detail-albums',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailAlbumsComponent implements OnInit {
 
-  constructor() { }
+  nameArtist: any;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    ) { }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.nameArtist = params['name'];
+      // Redirect if no name.
+      if (!this.nameArtist) {
+        //this.router.navigate(["/"]);
+      }
+      console.log(params);
+    });
   }
 
 }
